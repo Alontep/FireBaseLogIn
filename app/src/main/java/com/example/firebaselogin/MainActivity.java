4c05f6bee2a826a5c5698407383c58496f22693c
Alontep <Alontepper07@gmail.com>
2024-03-12 23:30:09 +0200

    to do list(nothing new)

--- app/src/main/java/com/example/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/firebaselogin/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*to do list:
+        1) connect fireBase to project
+        2) learn how to make a login page
+         */
+        */
+         */
     }
 }
