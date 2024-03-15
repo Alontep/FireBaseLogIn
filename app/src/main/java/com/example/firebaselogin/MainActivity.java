b83faf3becac572e5ba3a24e8ff4b5503387c31f
Alontep <Alontepper07@gmail.com>
2024-03-15 11:07:10 +0200

    finish fire base help, now video

--- app/src/main/java/com/example/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/firebaselogin/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         2) learn how to make a login page
          */
 
-         .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
+        .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
                 .setSupported(true)
                 // Your server's client ID, not your Android client ID.
                 .setServerClientId(getString(R.string.default_web_client_id))
@@ -33,5 +33,42 @@ public class MainActivity extends AppCompatActivity {
                 .build())
                 .build();
     }
+    private lateinit var auth: FirebaseAuth
+// ...
+// Initialize Firebase Auth
+            auth = Firebase.auth
+
+    paoverride fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = auth.getCurrentUser()
+        updateUI(currentUser);
     }
+    val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+    val idToken = googleCredential.googleIdToken
+    when {
+        idToken != null -> {
+            // Got an ID token from Google. Use it to authenticate
+            // with Firebase.
+            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+            auth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+        }
+    else -> {
+            // Shouldn't happen.
+            Log.d(TAG, "No ID token!")
+        }
+    }
+    Firebase.auth.signOut()
 }
